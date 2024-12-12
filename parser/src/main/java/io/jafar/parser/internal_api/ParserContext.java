09f4bf9195ac9e98c57cd5d629e673c3ebadbefe
Jaroslav Bachorik <j.bachorik@gmail.com>
2024-12-12 10:28:05 +0100

    Optimize deserialization and string loading

--- parser/src/main/java/io/jafar/parser/internal_api/ParserContext.java
+++ parser/src/main/java/io/jafar/parser/internal_api/ParserContext.java
@@ -3,9 +3,14 @@ package io.jafar.parser.internal_api;
 import io.jafar.parser.MutableConstantPools;
 import io.jafar.parser.MutableMetadataLookup;
 import io.jafar.parser.TypeFilter;
+import io.jafar.parser.internal_api.metadata.MetadataClass;
+import io.jafar.utils.CachedStringParser;
+import it.unimi.dsi.fastutil.longs.Long2ObjectMap;
 
 import java.lang.ref.WeakReference;
+import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
 
@@ -19,17 +24,65 @@ public final class ParserContext {
     private final Map<String, Class<?>> classTargetTypeMap = new ConcurrentHashMap<>();
     private final ConcurrentMap<String, WeakReference<?>> bag = new ConcurrentHashMap<>();
 
+    private Long2ObjectMap<Class<?>> classTypeMap = null;
+
+    public static class DeserializerKey {
+        private final long id;
+        private final String name;
+        private final String superType;
+        private final List<String> fieldNames;
+
+        public DeserializerKey(MetadataClass clz) {
+            this.id = clz.getId();
+            this.name = clz.getName();
+            this.superType = clz.getSuperType();
+            this.fieldNames = clz.getFields().stream().map(f -> f.getType().getName() + ":" + f.getName()).toList();
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            DeserializerKey that = (DeserializerKey) o;
+            return id == that.id && Objects.equals(name, that.name) && Objects.equals(superType, that.superType) && Objects.equals(fieldNames, that.fieldNames);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(id, name, superType, fieldNames);
+        }
+
+        @Override
+        public String toString() {
+            return "DeserializerKey{" +
+                    "id=" + id +
+                    ", name='" + name + '\'' +
+                    ", superType='" + superType + '\'' +
+                    ", fieldNames=" + fieldNames +
+                    '}';
+        }
+    }
+
+    private final ConcurrentMap<DeserializerKey, Deserializer<?>> globalDeserializerCache;
+
+    public final CachedStringParser.ByteArrayParser utf8Parser = CachedStringParser.byteParser();
+    public final CachedStringParser.CharArrayParser charParser = CachedStringParser.charParser();
+    public final byte[] byteBuffer = new byte[4096];
+    public final char[] charBuffer = new char[4096];
+
     public ParserContext() {
         this.metadataLookup = new MutableMetadataLookup();
         this.constantPools = new MutableConstantPools(metadataLookup);
+        this.globalDeserializerCache = new ConcurrentHashMap<>();
 
         this.typeFilter = null;
         this.chunkIndex = 0;
     }
 
-    public ParserContext(TypeFilter typeFilter, int chunkIndex, MutableMetadataLookup metadataLookup, MutableConstantPools constantPools) {
+    public ParserContext(TypeFilter typeFilter, int chunkIndex, MutableMetadataLookup metadataLookup, MutableConstantPools constantPools, ConcurrentMap<DeserializerKey, Deserializer<?>> deserializerCache) {
         this.metadataLookup = metadataLookup;
         this.constantPools = constantPools;
+        this.globalDeserializerCache = deserializerCache;
 
         this.typeFilter = typeFilter;
         this.chunkIndex = chunkIndex;
@@ -78,5 +131,17 @@ public final class ParserContext {
 
     public void bindDeserializers() {
         metadataLookup.bindDeserializers();
+    }
+
+    public void setClassTypeMap(Long2ObjectMap<Class<?>> map) {
+        classTypeMap = map;
+    }
+
+    public Long2ObjectMap<Class<?>> getClassTypeMap() {
+        return classTypeMap;
+    }
+
+    public ConcurrentMap<DeserializerKey, Deserializer<?>> getDeserializerCache() {
+        return globalDeserializerCache;
     }
 }
